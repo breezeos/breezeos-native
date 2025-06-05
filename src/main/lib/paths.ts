0b968bb59c082d275452fc3d7f2b40c4bab60d0a
cyplucastero <dangchankhang@gmail.com>
2025-06-05 18:37:47 +0700

    Update paths.ts

--- src/main/lib/paths.ts
+++ src/main/lib/paths.ts
@@ -2,7 +2,7 @@ import path from "path";
 
 /*
   Due to the configuration of latest versions of Electron React Boilerplate,
-  we can't use __dirname to return the src directory we wanted as it
+  we can't use __dirname directly to return the src directory path as it
   points to `.erb/dll`. This issue here is only present in the main process.
 */
 export const SRC_PATH = path.join(__dirname, "../../src");
